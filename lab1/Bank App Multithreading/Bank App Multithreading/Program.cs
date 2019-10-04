9c7753f16c1b2b55d747d5ffe615fa2330a3db76
Vlad David <vladdavidapps@gmail.com>
2019-10-04 15:37:35 +0300

    added lab1 start

---
+++ lab1/Bank App Multithreading/Bank App Multithreading/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_App_Multithreading
+{
+
+    class Account
+    {
+        String accountName;
+        Balance accountBalance;
+
+        public Account(String accountName)
+        {
+            this.accountName = accountName;
+            this.accountBalance = new Balance();
+            doStuff();
+        }
+
+        void doStuff()
+        {
+            Console.WriteLine("stuff");
+        }
+        
+    }
+
+    public class Balance
+    {
+        
+    }
+
+    public class Operation
+    {
+
+    }
+
+
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account("Vlad");
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
