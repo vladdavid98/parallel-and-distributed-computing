0edcb4e9c61050ffe073948d56942c3f5a52fd34
Vlad David <vladdavidapps@gmail.com>
2019-10-10 17:29:36 +0300

    modified lab1

--- lab1/Bank App Multithreading/Bank App Multithreading/Program.cs
+++ lab1/Bank App Multithreading/Bank App Multithreading/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -11,45 +12,32 @@ namespace Bank_App_Multithreading
 {
     internal class Program
     {
-        private static List<Account> accounts;
-        private static Mutex mut = new Mutex();
-
+        private static List<Account> _accounts;
+        private static readonly Mutex mut = new Mutex();
+        private static Random _random = new Random();
 
         private static Operation MakeRandomOperation()
         {
-            
-            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
-            var byteArray = new byte[4];
-            provider.GetBytes(byteArray);
-
-//            convert 4 bytes to an integer
-            int randomInteger = BitConverter.ToInt32(byteArray, 0);
-            if (randomInteger < 0) randomInteger *= -1;
-
-
-            Random random = new Random();
-
-
 //            get random account number
-            int randomNumber1 = random.Next(0, accounts.Count()); 
-            int randomNumber2 = random.Next(0, accounts.Count());
-            int randomNumber3 = randomInteger%100;
+            int randomNumber1 = _random.Next(0, _accounts.Count()); 
+            int randomNumber2 = _random.Next(0, _accounts.Count());
+            int randomNumber3 = _random.Next(0, 1000);
 
-            while (randomNumber1 == randomNumber2 || randomNumber3 > accounts[randomNumber1].AccountBalance)
+            while (randomNumber1 == randomNumber2 || randomNumber3 > _accounts[randomNumber1].AccountBalance)
             {
                 
-                randomNumber1 = random.Next(0, accounts.Count());
-                randomNumber2 = random.Next(0, accounts.Count());
+                randomNumber1 = _random.Next(0, _accounts.Count());
+                randomNumber2 = _random.Next(0, _accounts.Count());
                 
             }
 
-            mut.WaitOne();
-            accounts[randomNumber1].AccountBalance -= randomNumber3;
-            accounts[randomNumber2].AccountBalance += randomNumber3;
-            mut.ReleaseMutex();
+//            mut.WaitOne();
+            _accounts[randomNumber1].AccountBalance -= randomNumber3;
+            _accounts[randomNumber2].AccountBalance += randomNumber3;
+//            mut.ReleaseMutex();
 
             //            accounts[randomNumber1].OperationLog.AddToLog(this);
-            return new Operation(accounts[randomNumber1],accounts[randomNumber2],randomNumber3);
+            return new Operation(_accounts[randomNumber1],_accounts[randomNumber2],randomNumber3);
 
         }
 
@@ -58,48 +46,63 @@ namespace Bank_App_Multithreading
             for (int i = 0; i <= howManyTimes; i++)
             {
                 Operation op = MakeRandomOperation();
-                accounts[0].OperationLog.AddToLog(op);
-                accounts[1].OperationLog.AddToLog(op);
+                _accounts[0].OperationLog.AddToLog(op);
+                _accounts[1].OperationLog.AddToLog(op);
             }
         }
 
         private static void ShowAllAccounts()
         {
-            for (int i = 0; i < accounts.Count(); i++)
+            for (int i = 0; i < _accounts.Count(); i++)
             {
-                Console.WriteLine(accounts[i].ToString());
+                Console.WriteLine(_accounts[i].ToString());
             }
         }
 
 
         private static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             const int numberOfThreads = 10;
-            
-//            each thread can randomly transfer funds (make operations)
+            List<Thread> workerThreads = new List<Thread>();
+
+            //            each thread can randomly transfer funds (make operations)
             Account acc1 = new Account("Vlad", 2000,1);
             Account acc2 = new Account("Natalia", 2000,2);
 //            Account acc3 = new Account("Bogdan", 2000);
 //            Account acc4 = new Account("Ileana", 2000);
 //            Account acc5 = new Account("Radu", 2000);
 
-            accounts = new List<Account>() {acc1,acc2};
+            _accounts = new List<Account>() {acc1,acc2};
 
 
 
             for(int i = 0; i < numberOfThreads; i++)
             {
-                Thread myNewThread = new Thread(() => DoOperations(10000/numberOfThreads));
+                Thread myNewThread = new Thread(() => DoOperations(62000/numberOfThreads));
+                workerThreads.Add(myNewThread);
                 myNewThread.Start();
             }
+            
 
-
-            Thread.Sleep(2000);
 
             
+            foreach (Thread thread in workerThreads)
+            {
+                thread.Join();
+            }
+
             ShowAllAccounts();
+            sw.Stop();
+            Console.WriteLine("Elapsed={0}", sw.Elapsed);
 
             Console.ReadKey();
+
+
+            
+
+            
         }
 
         
