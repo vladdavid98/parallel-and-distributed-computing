b8c5f2e7d3f118e036b7b7850fa39cce8114f533
Vlad David <vladdavidapps@gmail.com>
2019-10-14 15:10:13 +0300

    modif

---
+++ lab2/MatrixOperations/MatrixOperations/Matrix.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MatrixOperations
+{
+    class Matrix
+    {
+        //private int[,] matrixElements;
+        public int Row { get; set; }
+        public int Column { get; set; }
+        private readonly int[,] matrixElements;
+
+        // int[,] array2D = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+        public Matrix(int[,] matrixElems)
+        {
+            this.matrixElements = matrixElems;
+            Row = matrixElems.GetLength(0);
+            Column = matrixElems.GetLength(1);
+        }
+
+        public int PutElem(int line, int column, int newValue)
+        {
+            int oldValue = matrixElements[line, column];
+            matrixElements[line, column] = newValue;
+            return oldValue;
+        }
+
+        public int GetElem(int line, int column)
+        {
+            return matrixElements[line, column];
+        }
+
+        public int GetLength(int dimension)
+        {
+            return matrixElements.GetLength(dimension);
+        }
+
+        public Matrix RandomValues()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < Row; i++)
+            for (int j = 0; j < Column; j++)
+                matrixElements[i, j] = rnd.Next(10);
+            return this;
+        }
+
+        public int[] GetColumn(int i)
+        {
+            var res = new int[Row];
+            for (int j = 0; j < Row; j++)
+                res[j] = matrixElements[j, i];
+            return res;
+        }
+
+        public int[] GetRow(int i)
+        {
+            var res = new int[Column];
+            for (int j = 0; j < Column; j++)
+                res[j] = matrixElements[i, j];
+            return res;
+        }
+
+        public int this[int i, int j]
+        {
+            get => matrixElements[i, j];
+            set => matrixElements[i, j] = value;
+        }
+
+
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            
+            var nrThreads = 1;
+            Matrix result = new Matrix(new int[a.Row, b.Column]);
+            var threads = new List<Thread>();
+            for (int i = 0; i < a.Row * b.Column; i++)
+            {
+                // int tempi = i;
+                // Thread thread = new Thread(() => VectorMult(tempi, a, b, result));
+                // thread.Start();
+                // threads.Add(thread);
+
+            }
+
+            foreach (Thread t in threads)
+                t.Join();
+            return result;
+        }
+
+        // TODO: STUFF
+        public static void MultiplyBetweenIndexes(int tmp1, int tmp2, Matrix a, Matrix b, Matrix result)
+        {
+            for (int tmp = tmp1; tmp < tmp2; tmp++)
+            {
+                int i = tmp / b.Column;
+                int j = tmp % b.Column;
+                var x = a.GetRow(i);
+                var y = b.GetColumn(j);
+                for (int k = 0; k < x.Length; k++)
+                    result[i, j] += x[k] * y[k];
+            }
+        }
+
+        public static void VectorMult(int tmp, Matrix a, Matrix b, Matrix result)
+        {
+            int i = tmp / b.Column;
+            int j = tmp % b.Column;
+            var x = a.GetRow(i);
+            var y = b.GetColumn(j);
+            for (int k = 0; k < x.Length; k++)
+                result[i, j] += x[k] * y[k];
+            //Console.WriteLine("Calculate element{0}{1}", i, j);
+        }
+
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < matrixElements.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixElements.GetLength(1); j++)
+                {
+                    s += matrixElements[i,j];
+                    s += " ";
+                }
+
+                s += "\n";
+            }
+
+            return s;
+        }
+    }
+}
---
+++ lab2/MatrixOperations/MatrixOperations/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MatrixOperations
+{
+    class Program
+    {
+        /*private static Matrix MatrixSum(Matrix a, Matrix b)
+        {
+            if(a.GetLength(0)!=b.GetLength(0) || a.GetLength(1)!=b.GetLength(1)) throw new Exception("Matrix sizes impossible for summation.");
+            Matrix finalMatrix = new Matrix(new int[a.GetLength(0), a.GetLength(1)]);
+            for (int i = 0; i < a.GetLength(0); i++)
+            for (int j = 0; j < a.GetLength(1); j++)
+                finalMatrix.PutElem(i, j, a.GetElem(i, j) + b.GetElem(i, j));
+            return finalMatrix;
+        }
+
+        private static Matrix MatrixMultiplication(Matrix a, Matrix b)
+        {
+            if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Matrix sizes impossible for multiplication.");
+            Matrix finalMatrix = new Matrix(new int[a.GetLength(0),b.GetLength(1)]);
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                // do for every line in matrix a
+
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    // do for element j in line i
+                    //                finalMatrix.PutElem(i, j, a.GetElem(i, j) + b.GetElem(i, j));
+
+
+
+
+                }
+            }
+
+
+            return finalMatrix;
+        }*/
+
+//        1,2
+//        3,4
+//        5,6
+//        7,8
+//        2,1
+//
+//        1,2,3,4
+//        5,6,7,8
+
+
+        static void Main(string[] args)
+        {
+            /*Matrix m1 = new Matrix(new int[,] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+            Matrix m2 = new Matrix(new int[,] {{3, 2, 1}, {6, 5, 4}, {3, 3, 3}});
+            Matrix m3 = new Matrix(new int[,] {{1, 2}, {3, 4}, {5, 6}, {7, 8}, {2, 1}});
+            Matrix m4 = new Matrix(new int[,] {{1, 2, 3, 4}, {5, 6, 7, 8}});
+            Console.WriteLine(m1+"\n"+m2+"\n"+m3+"\n"+m4);
+
+            Console.WriteLine(m1 + "+\n" + m2 +"=");
+            Console.WriteLine(MatrixSum(m1, m2).ToString());
+            Console.WriteLine(MatrixMultiplication(m3, m4).ToString());*/
+
+            Console.WriteLine("Input number of rows for matrix 1");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input number of columns for matrix 1");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input number of columns for matrix 2");
+            int k = int.Parse(Console.ReadLine());
+
+            Matrix A = new Matrix(new int[n, m]).RandomValues();
+            Matrix B = new Matrix(new int[m, k]).RandomValues();
+
+            Console.WriteLine(A.ToString());
+            Console.WriteLine(new String('-', 20));
+            Console.WriteLine(B.ToString());
+            Console.WriteLine(new String('-', 20));
+            Matrix C = A * B;
+            Console.WriteLine(C.ToString());
+
+
+            Console.ReadKey();
+        }
+
+
+
+
+        /*class Program
+        {
+            static void Main(string[] args)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int m = int.Parse(Console.ReadLine());
+                int k = int.Parse(Console.ReadLine());
+                Matrix A = new Matrix(n, m).RandomValues();
+                Matrix B = new Matrix(m, k).RandomValues();
+                A.Print();
+                Console.WriteLine(new String('-', 20));
+                B.Print();
+                Console.WriteLine(new String('-', 20));
+                Matrix C = A * B;
+                C.Print();
+
+
+                Console.ReadKey();
+        }
+        }*/
+
+
+
+    }
+
+
+
+}
